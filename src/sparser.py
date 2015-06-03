734e21de54b18c666f0a476b44788d8be3507971
Alexis Jacq <alexis.jacq@epfl.ch>
2015-06-03 20:20:01 +0200

    better loops

--- src/sparser.py
+++ src/sparser.py
@@ -1,12 +1,32 @@
-phrase = 'phrase test 1 , phrase test 2 . test 1 , phrase 2 , test 1 2'
+# -*- coding: utf-8 -*-
+
+phrase = "Cetait un soir de pluie . Lenfant etait seul . Bien que , ce ne fut plus un enfant .  Mais au font de sa poche , il avait su preserver quelques reves . Et puis deux-trois sous . Et puis un morceau de pain rance .   Il ne lui en restait plus beaucoup .  Or il faisait nuit . Et comme ses semblables cherchaient au sol pitances et miettes despoir , lui cherchait au ciel des traces detoiles . Mais la pluie mouillait ses yeux gris . Le vent tordait son ame . La gravite le clouait a une terre hostile et froide .  Nul ne pouvait le voir . Il etait trop maigre .   Mais quand il vous souriait , votre cœur le sentait , chaleur tendre entre deux bourrasques .   En allant ainsi , pousse par le hasard ,il senfonçait dans la ville .  La femme ny etait pour rien .Elle naurait pas pu leviter . Personne naurait pu . Car soudain , a la sortie dun virage , elle le percuta .   Pour elle , se fut une vague caresse ou meme , lidee dune caresse . Pour lui , le choc fut terriblement violant . Le cœur de la femme avait brise le sien . La beaute de la femme avait brûle sa laideur . Et les quelques reves quil gardait dans sa poche etaient tombes dans la boue . La femme , pressee par son elan , continua sa route . Insoucieuse , elle pietina les reves gisants , avant de disparaître .  Prive du peu qui lui restait ,lenfant semietta et le temps leparpilla comme de la quelconque cendre .  Une petite fille heureuse courrait , suivit par sa mere , bienveillante . Ses yeux , qui savaient les details invisibles ,trouverent les reves perdus . Dans un rire eclatant de joie simple ,elle les ramassa pour les montrer a sa maman .  Celle-ci fit une grimasse et gifla les mains salies de la petite pour en ejecter les reves et la boue .  La boue eclaboussa une vitrine .  Les reves gagnerent les cieux .  Entre deux nuages gris brillerent faiblement quelques etoiles , insaisissables ."
 
 import re
 import operator
+import random
+
+def cmp(c1, c2):
+    a,b = c1
+    c,d = c2
+    s = b+d
+    r = random.uniform(0,s)
+    return 1 if r>b else -1
+
+
+def random_pull(distribution): # dist. is a list of coulpes
+    if distribution :
+        sorted_couples = sorted(distribution,cmp)
+        return sorted_couples[0][0]
+    else:
+        return None
+
+
 
 splited = re.split('[ \-]+',phrase )
 
 TAU = 2
-RANGE = 20
+RANGE = 22
 
 word_followings = {} # word : list of (word/concept, relative position) + weights
 
@@ -40,7 +60,7 @@ for word1 in splited:
     for word2 in splited[i+1:limit]:
         j+=1
         a = treat(word1,word2,word_followings,j)
-        if a > TAU:
+        if a > 0:
             concept = "(%s)_0_(%s)_%i"%(word1,word2,j)
             b = treat(word1,concept,concept_followings,j)
             #concept_keys[concept] = {0:word1,j:word2}
@@ -52,9 +72,10 @@ for word1 in splited:
 for deep in range(2):
     print "%i ... "%deep
 
+    TAU = TAU+1
     # concept ->
     for i in range(len(global_layer)-1):
-            for concept1 in global_layer[i]:
+            for concept1 in frozenset(global_layer[i]):
 
                 # concept -> concept
                 limit = min(i+1+RANGE,len(global_layer))
@@ -69,19 +90,24 @@ for deep in range(2):
                             concept_clock[concept] = 0
                             global_layer[i].append(concept)
                             
-                
-                # concept -> word
-                j = 0
-                for word in splited[i+1:limit]:
-                    j+=1
-                    a = treat(concept1,word,concept_word,j)
-                    if a > TAU:
-                        concept = "(%s)_0_(%s)_%i"%(concept1,word,j)
-                        b = treat(concept1,concept,concept_followings,j)
-                        #concept_keys[concept] = {0:concept1,j:concept2}
-                        concept_keys[concept] = (j,word,b)
-                        concept_clock[concept] = 0
-                        global_layer[i].append(concept)
+# concept ->
+for i in range(len(global_layer)-1):
+        for concept1 in global_layer[i]:
+
+            
+            # concept -> word
+            j = 0
+            limit = min(i+1+RANGE,len(global_layer))
+            for word in splited[i+1:limit]:
+                j+=1
+                a = treat(concept1,word,concept_word,j)
+                if a > 2:
+                    concept = "(%s)_0_(%s)_%i"%(concept1,word,j)
+                    b = treat(concept1,concept,concept_followings,j)
+                    #concept_keys[concept] = {0:concept1,j:concept2}
+                    concept_keys[concept] = (j,word,b)
+                    concept_clock[concept] = 0
+                    global_layer[i].append(concept)
 
         # word ->
         #i=0
@@ -89,21 +115,21 @@ for deep in range(2):
 
             
 print "       ################# TRAINING :"
-print word_followings
-print "____________"
-print concept_followings
-print "____________"
+#print word_followings
+#print "____________"
+#    print concept_followings
+#    print "____________"
 print splited
-print '____________'
-print concept_keys
+#    print '____________'
+#print concept_keys
 print
 print "       ################## SAMPLING :"
 
-current_word = 'phrase'
+current_word = '.'
 current_pos = 0
 result = [current_word]
 
-length = 20
+length = 10
 
 word_score = {} # position(global), word : score
 concept_score = {} # position(global), concept : score
@@ -116,7 +142,7 @@ potential_concepts = {} # concept, position when score>0
 def score_update(concept, pos, score, score_dict):
     if pos in score_dict:
         if concept in score_dict:
-            scor_dict[pos][concept] += score
+            score_dict[pos][concept] += score
         else:
             score_dict[pos].update({concept:score})
     else:
@@ -127,7 +153,7 @@ for i in range(length):
     # word -> word :
     for word in word_followings[current_word]:
         for pos in word_followings[current_word][word]:
-            score_update(word,i+pos,word_followings[current_word][word][pos],word_score)
+            score_update(word,i+pos,2*word_followings[current_word][word][pos],word_score)
 
     # word -> concept :
     if current_word in concept_followings:
@@ -152,7 +178,9 @@ for i in range(length):
                     for pos in concept_word[concept1][word]:
                         score_update(word,i+pos,concept_word[concept1][word][pos],word_score)
 
-    current_word = max(word_score[i+1].iteritems(), key=operator.itemgetter(1))[0]
+    distribution = word_score[i+1].items()
+    current_word = random_pull(distribution)
+    #current_word = max(word_score[i+1].iteritems(), key=operator.itemgetter(1))[0]
     result.append(current_word)
 
 print result
